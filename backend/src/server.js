e7dc03d7f01acc576ab0a5f493cacff13772a9b5
MrMorales <mrphysics3.14@gmail.com>
2024-10-21 14:33:10 -0400

    "Server up and running"

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { dirname } from "path";
+// import { dirname } from "path";
 
 // initializing the express app and setting up the port, path and log variables
 const app = express();
 const log = console.log;
-const path = dirname(fileURLToPath(import.meta.url));
-const port = 3001;
+// const path = dirname(fileURLToPath(import.meta.url));
+const port = 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -16,5 +16,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.listen(port, () => {
-  log("server running successfully on port 3001 sir !");
+  log("server running successfully on port 3000 sir !");
 });
